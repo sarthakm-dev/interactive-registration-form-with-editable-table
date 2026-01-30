b4c59f12977d8d434675d7c026927096e370e698
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-01-30 14:16:09 +0530

    feat(theme): add antd theme alogorithm

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import Form from '../features/survey/components/Form';
 import Table from '../features/survey/components/Table';
 import RatingsModal from '../features/survey/components/RatingsModal';
 import ThemeToggle from '../shared/components/ThemeToggle';
-import { ConfigProvider, Modal, Button } from 'antd';
+import { ConfigProvider, Modal, Button, theme as t } from 'antd';
 import { useTableStore } from '../store/useTableStore';
 import { useUIStore } from '../store/useUIStore';
 import { useEffect } from 'react';
@@ -11,6 +11,7 @@ function App() {
   const { viewRow, setViewRow, deleteTarget, confirmDelete, setDeleteTarget } = useTableStore();
   const { showSuccess, closeSuccess, successType, showError, errorMessage, closeError } =
     useUIStore();
+  const themeMode = useUIStore((s) => s.theme);
   const theme = useUIStore((s) => s.theme);
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
@@ -22,6 +23,7 @@ function App() {
           Radio: { colorPrimary: 'green' },
           Button: { colorPrimary: 'green' },
         },
+        algorithm: themeMode === 'dark' ? t.darkAlgorithm : t.defaultAlgorithm,
       }}
     >
       <div className="app-container">
